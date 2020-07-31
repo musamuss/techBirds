534f5133c3226ecea04b98d3a3a80dde7bd5ad3b
Musa Deniev <m.deniev@zedplus.ru>
2020-07-31 19:04:54 +0300

    Initial Commit

---
+++ Code/techBirds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techBirds
+//
+//  Created by admin on 31.07.2020.
+//  Copyright © 2020 techBirds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
