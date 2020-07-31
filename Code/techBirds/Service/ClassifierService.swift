0f19cc3170f26b0956ffc8b1ff082bc2f86382d2
Artem Belkov <bestk1ngarthur@aol.com>
2020-07-31 19:27:56 +0300

    Add app singleton

---
+++ Code/techBirds/Service/ClassifierService.swift
@@ -0,0 +1,13 @@
+//
+//  ClassifierService.swift
+//  techBirds
+//
+//  Created by Artem Belkov on 31.07.2020.
+//  Copyright © 2020 techBirds. All rights reserved.
+//
+
+import Foundation
+
+class ClassifierService {
+    
+}
