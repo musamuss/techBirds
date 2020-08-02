4765726cc4fc295307351aaecddd025c5cef69cc
musamuss <45773032+musamuss@users.noreply.github.com>
2020-08-02 11:53:21 +0300

    Demo branch (#25)
    * demo
    
    * Update DemoViewController.swift
    
    * presentation
    
    Co-authored-by: Musa Deniev <m.deniev@zedplus.ru>

---
+++ Code/techBirds/VC/DemoViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DemoViewController.swift
+//  techBirds
+//
+//  Created by admin on 02.08.2020.
+//  Copyright © 2020 techBirds. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    @IBOutlet weak var enterComment: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonAction() {
+        if let text = enterComment.text {
+            let category = composeNiceText(App.current.categoriesClassifier.classifyText(text))
+            let team = composeNiceText(App.current.teamsClassifier.classifyText(text))
+            resultLabel.text = "\(category)\n\(team)"
+        }
+        
+        
+    }
+    
+    private func composeNiceText(_ dict: [String: Double]) -> String {
+        var text = ""
+        let dictValInc = dict.sorted(by: { $0.value > $1.value })
+        
+        for i in dictValInc {
+            text.append("\(i.key): \(i.value)\n")
+        }
+        return text
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
